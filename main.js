e2fb52c0746c9933910bc82c7b6129f7ca26a04a
JonathanLaureano <JonathanLaureano20@gmail.com>
2022-01-19 14:36:48 -0500

    commit tree a592f49c00a6797ee2b1da75454a76eacc8f9955

--- main.js
+++ main.js
@@ -23,3 +23,29 @@ function mimicServerCall(url="http://mimicServer.example.com", config={}) {
     }, 300);
   });
 }
+
+
+//Event Listener
+const errModal = document.querySelector('#modal')
+errModal.classList.add("hidden")
+const theHearts = document.querySelectorAll('.like-glyph')
+theHearts.forEach(like => {
+  like.addEventListener('click', (e) => {
+    const heart = e.target;
+    mimicServerCall()
+      .then(() => {
+      if ( heart.innerText === EMPTY_HEART) {
+        heart.innerText = FULL_HEART;
+        heart.className = "activated-heart";
+      } else {
+        heart.innerText = EMPTY_HEART;
+        heart.className = "";
+      }
+    })
+      .catch((error) => {
+      const modal = document.getElementById("modal");
+      modal.className = "";
+      modal.innerText = error;
+      setTimeout(() =>  modal.className = "hidden", 3000);
+    });
+  })})
